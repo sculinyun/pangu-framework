828f9c2afc7e80d88313832810d6f16579d486e6
youzhensheng <youzhensheng@hbscjt.com.cn>
2023-10-11 11:07:03 +0800

    fix:解决单服务调用跨域问题，方便调试

--- pangu-core/hbcsjt-app-spring-boot-starter-web/src/main/java/cn/com/hbscjt/app/framework/web/config/WebAutoConfiguration.java
+++ pangu-core/hbcsjt-app-spring-boot-starter-web/src/main/java/cn/com/hbscjt/app/framework/web/config/WebAutoConfiguration.java
@@ -1,5 +1,6 @@
 package cn.com.hbscjt.app.framework.web.config;
 
+import cn.com.hbscjt.app.framework.common.enums.WebFilterOrderEnum;
 import cn.com.hbscjt.app.framework.redis.core.RedisService;
 import cn.com.hbscjt.app.framework.web.core.convert.MvcDateConvert;
 import cn.com.hbscjt.app.framework.web.core.handler.GlobalExceptionHandler;
@@ -11,10 +12,16 @@ import cn.com.hbscjt.app.framework.web.core.util.WebFrameworkUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 /**
  * @author : ly
@@ -44,5 +51,29 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new MvcDateConvert());
+    }
+
+
+    private static <T extends Filter> FilterRegistrationBean<T> createFilterBean(T filter, Integer order) {
+        FilterRegistrationBean<T> bean = new FilterRegistrationBean<>(filter);
+        bean.setOrder(order);
+        return bean;
+    }
+
+    /**
+     * 创建 CorsFilter Bean，解决跨域问题
+     */
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterBean() {
+        // 创建 CorsConfiguration 对象
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 设置访问源地址
+        config.addAllowedHeader("*"); // 设置访问源请求头
+        config.addAllowedMethod("*"); // 设置访问源请求方法
+        // 创建 UrlBasedCorsConfigurationSource 对象
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 对接口配置跨域设置
+        return createFilterBean(new CorsFilter(source), WebFilterOrderEnum.CORS_FILTER);
     }
 }
