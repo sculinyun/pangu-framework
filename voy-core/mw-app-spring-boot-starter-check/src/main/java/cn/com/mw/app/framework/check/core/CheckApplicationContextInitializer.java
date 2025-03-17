1fd4cc102d5d11e78a6a9f19748d8ac13ddbd7b1
linyun011 <linyun011@h-world.com>
2025-03-17 15:59:22 +0800

    调试应用

---
+++ voy-core/mw-app-spring-boot-starter-check/src/main/java/cn/com/mw/app/framework/check/core/CheckApplicationContextInitializer.java
@@ -0,0 +1,21 @@
+package cn.com.mw.app.framework.check.core;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author : ly
+ * @date : 2024-03-07 16:49
+ **/
+@Slf4j
+@RequiredArgsConstructor
+public class CheckApplicationContextInitializer implements ApplicationContextInitializer {
+    private final CheckUtil checkUtil;
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        log.info("[voy-framework]开启参数检查");
+        checkUtil.checkAll();
+    }
+}
